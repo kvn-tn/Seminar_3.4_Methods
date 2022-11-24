e19a11f5b1d5d198310cd1f39801c7850a4b383e
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-24 20:05:34 +0300

    Task 29_выводит массив из 8 элементов

---
+++ HOMEWORK/Task29_arrayof8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] GetArray(int [] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+    array[i] = new Random().Next(0,7);
+   }
+        return array;
+}
+
+int [] array = new int [8];
+GetArray(array);
+
+Console.Write(String.Join(",", array));
