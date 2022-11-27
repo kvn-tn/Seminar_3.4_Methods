55bdc143fa0d00be261dcf5fa0a1cd25f84f8e37
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-27 21:02:37 +0300

    Task35_3х_знач_количество чётных чисел в массиве

---
+++ HW_Seminar3.5/Task34_QttEvenNum/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int [] array = {345, 897, 568, 234, 100};
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]%2 ==0)
+    count ++;       
+}
+
+ Console.WriteLine("Количество цифр "+count);
+
+
+
+// int [] array = {345, 897, 568, 234, 100};
+// int [] array2 = new int [array.Length];
+// int count = 0;
+
+// int [] GetArray(int[] array2)
+// {
+// for (int i = 0; i < array.Length; i++)
+//     {
+//     array2[i]=new Random().Next(100,999);
+//     if(array2[i]%2 ==0)
+//     count ++;       
+//     }
+//     return array2;
+// }
+// GetArray(array2);
+// Console.WriteLine(String.Join(" ",array2));
+
+// Console.WriteLine("Количество цифр "+count);
