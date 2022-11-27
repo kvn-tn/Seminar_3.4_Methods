401b07be3291b2d377785e778d329dff94512c91
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-27 15:30:23 +0300

    Task27_выдаёт сумму цифр в числе

---
+++ HOMEWORK/Task27_SumofNum/Program.cs
@@ -0,0 +1,21 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
+
+int GetSumNum(int res)
+{
+    int sum =0;
+    while(res>0)
+    {
+        sum+=res % 10;
+        res = res / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetSumNum(num));
