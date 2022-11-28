802a74a3b764c4f22b48d04f3a3de4cae4f690b9
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-28 21:02:28 +0300

    Task 42 преобразовние десятичного числа в двоичное

---
+++ SEMINAR 3.6_C#_ArrayCont/Practice42_10ishTo2ish/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101 // делим на 2 и остатки в обратном порядке записываем
+// 3  -> 11
+// 2  -> 10
+
+// string result = ""; //-  пустой символ
+
+
+Console.Write(" N =  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string result ="";
+while(n>0)
+{
+    result=n%2+result;
+
+    n/=2;
+
+}
+Console.Write(result);
