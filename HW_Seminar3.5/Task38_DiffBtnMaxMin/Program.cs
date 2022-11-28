8f27adda4d2c88608db1b6f1944a11eca0d34227
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-28 20:42:21 +0300

    Task38_разница между макс и мин элеметами массива_v4_new code

--- HW_Seminar3.5/Task38_DiffBtnMaxMin/Program.cs
+++ HW_Seminar3.5/Task38_DiffBtnMaxMin/Program.cs
@@ -43,3 +43,45 @@ for (int i = 0; i < array.Length; i++)
 Console.WriteLine(("Максимальное значение = " + MaxNum) + " " + ("Mинимальное значение = " + MinNum));
 Console.WriteLine("Разница = " + (MaxNum - MinNum));
 
+// Console.Clear();
+// Console.WriteLine("Введите желаемое кол-во элементов массива N ");
+// Console.Write(" N =  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// double[] num = SetArrayRealNumbers(n);
+
+// double min = num[0];
+// double max = num[0];
+
+// for (int i = 0; i < n; i++)
+// {
+//     if (num[i] >= max)
+//     {
+//         max = num[i];
+//     }
+//     else if (num[i] <= min)
+//     {
+//         min = num[i];
+//     }
+// }
+
+// Console.Write(String.Join("; ", num));
+
+// Console.WriteLine();
+
+// Console.WriteLine($" В данном массиве разница между максимальным ({max}) и минимальным ({min})\n 
+//элементом с учетом знака будет равна max - min  = {Math.Round((max - min), 2)}");
+
+//  double[] SetArrayRealNumbers (int n)
+// {
+//     double[] num = new double[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         num[i] = new Random().Next(100, 1000) + new Random().NextDouble(); // все числа после запятой указаны 
+//во второй части (после плюса)
+
+//         num[i] = Math.Round(num[i], 2); // функция указывает сколько элементов отсечь после запятой
+//     }
+//     return num;
+// }
