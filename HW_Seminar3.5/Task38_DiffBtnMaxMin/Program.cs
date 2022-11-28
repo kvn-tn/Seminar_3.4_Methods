3a2abb4d5a698009f42b394b241aedea252542b6
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-28 20:28:09 +0300

    Task38_разница между макс и мин элеметами массива_v3_new code

--- HW_Seminar3.5/Task38_DiffBtnMaxMin/Program.cs
+++ HW_Seminar3.5/Task38_DiffBtnMaxMin/Program.cs
@@ -3,21 +3,43 @@
 
 // [3 7 22 2 78] -> 76
 
-double[] array = {3, 7, 22, 2, 78, 123, .6};
-double MaxNum = array[0];
-double MinNum = array[0];
+// double[] array = {3, 7, 22, 2, 78, 123, .6};
+// double MaxNum = array[0];
+// double MinNum = array[0];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i]>MaxNum)
+//     {
+//         MaxNum = array[i];
+//     }
+//       if (array[i]<MinNum)
+//     {
+//         MinNum = array[i]; 
+//     }
+// }
+
+// Console.WriteLine(("Максимальное значение = "+MaxNum)+" "+("Mинимальное значение = "+MinNum));
+// Console.WriteLine("Разница = "+(MaxNum - MinNum));
+
+int[] array = new int[8];
+int MaxNum = int.MinValue; // функция считает переменные как положительные так и отрицательные. 
+// Сама переменная самая большая, все остальные будут меньше.
+int MinNum = int.MaxValue;
 
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i]>MaxNum)
+    array[i] = new Random().Next(1, 10);
+    if (array[i] > MaxNum)
     {
         MaxNum = array[i];
     }
-      if (array[i]<MinNum)
+    if (array[i] < MinNum)
     {
-        MinNum = array[i]; 
+        MinNum = array[i];
     }
 }
 
-Console.WriteLine(("Максимальное значение = "+MaxNum)+" "+("Mинимальное значение = "+MinNum));
-Console.WriteLine("Разница = "+(MaxNum - MinNum));
+Console.WriteLine(("Максимальное значение = " + MaxNum) + " " + ("Mинимальное значение = " + MinNum));
+Console.WriteLine("Разница = " + (MaxNum - MinNum));
+
