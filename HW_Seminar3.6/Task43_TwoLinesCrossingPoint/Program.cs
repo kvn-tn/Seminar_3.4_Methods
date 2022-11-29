8e326078df7be60c7569896272f75b28753bf89f
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-29 19:40:49 +0300

    Task38_разница между макс и мин элеметами массива_v2

---
+++ HW_Seminar3.6/Task43_TwoLinesCrossingPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double resultX = (b1-b2) / (k2-k1); 
+double resultY = (k2*b1-k1*b2)/(k2-k1); 
+
+Console.WriteLine($"Точка перечечения X: {resultX}, Y: {resultY}");
