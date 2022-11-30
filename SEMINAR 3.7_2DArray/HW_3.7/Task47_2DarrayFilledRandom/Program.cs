647a779b7c70b23d91eb1d400bb1efb217c84fca
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-30 17:35:39 +0300

    Task47_двумерный массив из случайных вещественных чисел

---
+++ SEMINAR 3.7_2DArray/HW_3.7/Task47_2DarrayFilledRandom/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите кол-во столбцов ");
+var columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во строк ");
+var rows = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[columns,rows];
+
+for (var i = 0; i < matrix.GetLength(0); i++)
+{
+    for (var j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(-9,9)+ new Random().NextDouble();
+        Console.Write(Math.Round(matrix[i,j],1)+" ");
+    
+        }
+    Console.WriteLine();
+}
