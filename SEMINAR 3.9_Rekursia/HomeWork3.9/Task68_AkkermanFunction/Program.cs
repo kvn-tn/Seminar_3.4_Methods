b6d70dea98ffde999716de580fbba74ae01cdafd
Tatiana Kvon <kvn-tn@mail.ru>
2022-12-04 14:20:49 +0300

    HW Task68_Вычисление функции Аккермана для 2 неотрицательных чисел

---
+++ SEMINAR 3.9_Rekursia/HomeWork3.9/Task68_AkkermanFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число M: ");
+int m =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n =Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+Console.WriteLine(AkkermanFunction(m,n)); 
+
+int AkkermanFunction(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return AkkermanFunction(n - 1, 1);
+    else
+      return AkkermanFunction(n - 1, AkkermanFunction(n, m - 1));
+}
